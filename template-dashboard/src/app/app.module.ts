d3eb9201c951adee5601e6d8243341723905aea6
Javier Izquierdo <javier.izquierdo.gomez@gmail.com>
2018-07-24 19:33:59 +0200

    refactor code

--- template-dashboard/src/app/app.module.ts
+++ template-dashboard/src/app/app.module.ts
@@ -2,8 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { NavComponent } from './nav/nav.component';
-import { FooterComponent } from './footer/footer.component';
+import { NavComponent } from './views/commons/nav/nav.component';
+import { FooterComponent } from './views/commons/footer/footer.component';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
