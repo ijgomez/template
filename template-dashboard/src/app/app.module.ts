6663811c17f12bcc300c024132380d64584ca530
Javier Izquierdo Gomez <javier.izquierdo.gomez@gmail.com>
2018-08-08 20:32:52 +0200

    remove code

--- template-dashboard/src/app/app.module.ts
+++ template-dashboard/src/app/app.module.ts
@@ -2,25 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { NavComponent } from './views/layout/nav/nav.component';
-import { FooterComponent } from './views/layout/footer/footer.component';
 import { AppRoutingModule } from './app-routing.module';
-import { DashboardComponent } from './views/dashboard/dashboard.component';
 import { ReportsModule } from './views/reports/reports.module';
 import { SupportModule } from './views/support/support.module';
 import { SecurityModule } from './views/security/security.module';
 import { MonitorsModule } from './views/monitors/monitors.module';
+import { DashboardComponent } from './views/dashboard/dashboard.component';
 import { MenuComponent } from './views/layout/menu/menu.component';
-import { ProfileComponent } from './views/layout/profile/profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent,
     MenuComponent,
-    FooterComponent,
-    DashboardComponent,
-    ProfileComponent
+    DashboardComponent
   ],
   imports: [
     BrowserModule, AppRoutingModule,
--- template-dashboard/src/app/views/reports/reports-routing.module.ts
+++ template-dashboard/src/app/views/reports/reports-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ReportHomeComponent } from './report-home/report-home.component';
+import { ReportListComponent } from './report-list/report-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ReportHomeComponent, children: [
+    { path: '', pathMatch: 'full', redirectTo: 'list'},
+    { path: 'list', component: ReportListComponent}
+  ]}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
