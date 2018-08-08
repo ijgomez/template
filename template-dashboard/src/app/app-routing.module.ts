2aed75adad9141cb68010892b6f3ec67d2441b0d
Javier Izquierdo Gomez <javier.izquierdo.gomez@gmail.com>
2018-08-08 18:59:45 +0200

    add code

--- template-dashboard/src/app/app-routing.module.ts
+++ template-dashboard/src/app/app-routing.module.ts
@@ -1,16 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './views/dashboard/dashboard.component';
-import { ReportsModule } from './views/reports/reports.module';
 
 const routes: Routes = [
-   { path: '', component: DashboardComponent},
-   { path: 'reports', loadChildren: 'app/views/reports/reports.module#ReportsModule'}
+   { path: '', component: DashboardComponent}
 ];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
-    exports: [ReportsModule]
+    exports: []
 })
 export class AppRoutingModule {}
 
