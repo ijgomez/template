44daa7c44b95c0e2f3b7a2a7f0f19f63ac984c14
Javier Izquierdo <javier.izquierdo.gomez@gmail.com>
2018-08-10 13:07:05 +0200

    dashboard

--- template-dashboard/src/app/app-routing.module.ts
+++ template-dashboard/src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './views/dashboard/dashboard.component';
 
 const routes: Routes = [
-    {path: '', redirectTo: '', pathMatch: 'full'},
+    {path: '', component: DashboardComponent},
     { path: 'reports',  loadChildren: 'app/views/reports/reports.module#ReportsModule'},
     { path: 'monitors', loadChildren: 'app/views/monitors/monitors.module#MonitorsModule'},
     { path: 'security', loadChildren: 'app/views/security/security.module#SecurityModule'},
