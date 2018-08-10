d8fa686e608c6fcca0a0c3ed876642f52ea82cb4
Javier Izquierdo <javier.izquierdo.gomez@gmail.com>
2018-08-10 13:48:04 +0200

    add forms navigation

--- template-dashboard/src/app/views/reports/reports-routing.module.ts
+++ template-dashboard/src/app/views/reports/reports-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ReportsListComponent } from './reports-list/reports-list.component';
 import { ReportsHomeComponent } from './reports-home/reports-home.component';
+import { ReportComponent } from './report/report.component';
 
 const routes: Routes = [
   { path: '', component: ReportsHomeComponent, children: [
-    { path: '', pathMatch: 'full', redirectTo: 'list'},
-    { path: 'list', component: ReportsListComponent}
+    //{ path: '', pathMatch: 'full', redirectTo: 'list'},
+    { path: '', component: ReportsListComponent},
+    { path: 'list', component: ReportsListComponent},
+    { path: 'create', component: ReportComponent},
+    { path: 'edit/:id', component: ReportComponent}
   ]}
 ];
 
