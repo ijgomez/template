5bdf4cb2dd66daf97199f660d4e6e77e95c8179a
Javier Izquierdo <javier.izquierdo.gomez@gmail.com>
2018-08-10 10:38:57 +0200

    base modules

--- template-dashboard/src/app/app-routing.module.ts
+++ template-dashboard/src/app/app-routing.module.ts
@@ -3,7 +3,10 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
     {path: '', redirectTo: '', pathMatch: 'full'},
-    { path: 'reports', loadChildren: 'app/views/reports/reports.module#ReportsModule'}
+    { path: 'reports',  loadChildren: 'app/views/reports/reports.module#ReportsModule'},
+    { path: 'monitors', loadChildren: 'app/views/monitors/monitors.module#MonitorsModule'},
+    { path: 'security', loadChildren: 'app/views/security/security.module#SecurityModule'},
+    { path: 'support',  loadChildren: 'app/views/support/support.module#SupportModule'}
 ];
 
 @NgModule({
---
+++ template-dashboard/src/app/views/monitors/monitors.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MonitorsRoutingModule } from './monitors-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    MonitorsRoutingModule
+  ],
+  declarations: []
+})
+export class MonitorsModule { }
