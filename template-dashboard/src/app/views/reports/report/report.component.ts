d10a3a6941e1c7cf7a7d619fc0154614b95e176d
Javier Izquierdo Gomez <javier.izquierdo.gomez@gmail.com>
2018-08-11 22:26:17 +0200

    report form

--- template-dashboard/src/app/views/reports/report/report.component.ts
+++ template-dashboard/src/app/views/reports/report/report.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { ReportsService } from '../../../services/reports/reports.service';
-import { FormGroup, FormControl, Validators, FormBuilder } from '../../../../../node_modules/@angular/forms';
+import { FormGroup, Validators, FormBuilder } from '../../../../../node_modules/@angular/forms';
 
 @Component({
   selector: 'app-report',
@@ -11,16 +11,35 @@ export class ReportComponent implements OnInit {
 
   reportForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder, private reportsService: ReportsService) { }
+  constructor(private formBuilder: FormBuilder, private cd: ChangeDetectorRef, private reportsService: ReportsService) { }
 
   ngOnInit() {
     this.reportForm = this.formBuilder.group({
       name: ['', Validators.required],
-      description: ['', Validators.required]
+      description: ['', Validators.required],
+      file: [null, Validators.required]
     });
   }
 
   onSubmit() {
     console.warn(this.reportForm.value);
+  }
+
+  onFileChange(event) {
+    const reader = new FileReader();
+
+    if (event.target.files && event.target.files.length) {
+      const [file] = event.target.files;
+      reader.readAsDataURL(file);
+
+      reader.onload = () => {
+        this.reportForm.patchValue({
+          file: reader.result
+       });
+
+        // need to run CD since file load runs outside of zone
+        this.cd.markForCheck();
+      };
+    }
   }
 }
