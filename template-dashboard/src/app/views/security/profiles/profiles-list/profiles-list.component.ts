30c47e30d4cc16aab892e85ba6345c144612d5a2
Javier Izquierdo Gomez <javier.izquierdo.gomez@gmail.com>
2018-08-12 22:32:28 +0200

    profile code

--- template-dashboard/src/app/views/security/profiles/profiles-list/profiles-list.component.ts
+++ template-dashboard/src/app/views/security/profiles/profiles-list/profiles-list.component.ts
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilesListComponent implements OnInit {
 
+  data: any[];
+
   constructor() { }
 
   ngOnInit() {
+    this.data = [
+      {id: 1, name: 'ADMIN', description: 'Administrator'},
+      {id: 2, name: 'SUPPORT', description: 'Support'},
+      {id: 3, name: 'USER', description: 'Operator'},
+      {id: 4, name: 'MANAGER', description: 'Manager'}
+    ];
   }
 
 }
--- template-dashboard/src/app/views/security/profiles/profiles.module.ts
+++ template-dashboard/src/app/views/security/profiles/profiles.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { ProfilesRoutingModule } from './profiles-routing.module';
 import { ProfilesListComponent } from './profiles-list/profiles-list.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   imports: [
     CommonModule,
     ProfilesRoutingModule
   ],
-  declarations: [ProfilesListComponent]
+  declarations: [ProfilesListComponent, ProfileComponent]
 })
 export class ProfilesModule { }
