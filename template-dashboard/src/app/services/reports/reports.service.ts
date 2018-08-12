2eb678fd4a4a934426455e544e3061fb07adca51
Javier Izquierdo Gomez <javier.izquierdo.gomez@gmail.com>
2018-08-12 22:11:18 +0200

    add report service code

--- template-dashboard/src/app/services/reports/reports.service.ts
+++ template-dashboard/src/app/services/reports/reports.service.ts
@@ -1,7 +1,105 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { Http, Response, RequestOptions, Headers, RequestMethod } from '@angular/http';
+import { Observable } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+import { Report } from '../../domain/reports/report';
+import { ReportCriteria } from '../../domain/reports/report-criteria';
 
 @Injectable()
 export class ReportsService {
 
-  constructor() { }
+  private url = environment.urlBase + '/reports';
+
+  private headers = new Headers({'Content-type': 'application/json'});
+
+  constructor(private http: Http) { }
+
+  findByCriteria(criteria: ReportCriteria): Observable<Report[]> {
+    const options = new RequestOptions({ headers: this.headers});
+
+    return this.http.post(this.url + '/search', criteria, options).pipe(
+      map((response: Response) => {
+        return response.json();
+      }),
+      catchError(this.handleError)
+    );
+  }
+
+  countByCriteria(criteria: ReportCriteria): Observable<number> {
+    const options = new RequestOptions({ headers: this.headers});
+
+    return this.http.post(this.url + '/count', criteria, options).pipe(
+      map((response: Response) => {
+        return response.json();
+      }),
+      catchError(this.handleError)
+    );
+  }
+
+  create(report: Report): Observable<number> {
+    const options = new RequestOptions({ headers: this.headers});
+
+    return this.http.post(this.url, report, options).pipe(
+      map(success => success.status),
+      catchError(this.handleError)
+    );
+  }
+
+  read(id: number | string): Observable<Report> {
+    const options = new RequestOptions({ headers: this.headers});
+
+    return this.http.get(`${this.url}/${id}`, options).pipe(
+      map((response: Response) => {
+        return response.json();
+      }),
+      catchError(this.handleError)
+    );
+  }
+
+  update(report: Report): Observable<number> {
+    const options = new RequestOptions({ headers: this.headers});
+
+    return this.http.put(`${this.url}/${report.id}`, report, options).pipe(
+      map((response: Response) => {
+        return response.status;
+      }),
+      catchError(this.handleError)
+    );
+  }
+
+  delete(report: Report): Observable<number> {
+    const options = new RequestOptions({ headers: this.headers});
+
+    return this.http.delete(`${this.url}/${report.id}`, options).pipe(
+      map((response: Response) => {
+        return response.status;
+      }),
+      catchError(this.handleError)
+    );
+  }
+
+  export(criteria: ReportCriteria): Observable<Response> {
+    const options = new RequestOptions({});
+
+    return this.http.get(this.url + '/export', options).pipe(
+      map((response: Response) => {
+        return response;
+      }),
+      catchError(this.handleError)
+    );
+  }
+
+  private handleError(error: Response | any) {
+    let errMsg: string;
+    if (error instanceof Response) {
+        const body = error.json() || '';
+        const err = body.error || JSON.stringify(body);
+        errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+    } else {
+        errMsg = error.message ? error.message : error.toString();
+    }
+    console.error('service error: ' + errMsg);
+    return Observable.throw(errMsg);
+  }
 }
--- template-dashboard/src/app/views/reports/reports.module.ts
+++ template-dashboard/src/app/views/reports/reports.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { HttpModule } from '@angular/http';
 
 import { ReportsRoutingModule } from './reports-routing.module';
 import { ReportsListComponent } from './reports-list/reports-list.component';
@@ -8,14 +9,15 @@ import { ReportsHomeComponent } from './reports-home/reports-home.component';
 import { ReportComponent } from './report/report.component';
 import { ReportExecuteComponent } from './report-execute/report-execute.component';
 import { ComponentsModule } from '../components/components.module';
+import { ReportsService } from '../../services/reports/reports.service';
 
 @NgModule({
   imports: [
-    CommonModule,
-    ReactiveFormsModule,
+    CommonModule, HttpModule, ReactiveFormsModule,
     ReportsRoutingModule,
     ComponentsModule
   ],
-  declarations: [ReportsListComponent, ReportsHomeComponent, ReportComponent, ReportExecuteComponent]
+  declarations: [ReportsListComponent, ReportsHomeComponent, ReportComponent, ReportExecuteComponent],
+  providers: [ReportsService]
 })
 export class ReportsModule { }
