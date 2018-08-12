b924c41feb855cd346f4175abff82ba7a65f74e3
Javier Izquierdo Gomez <javier.izquierdo.gomez@gmail.com>
2018-08-12 21:42:36 +0200

    test component module

--- template-dashboard/src/app/views/components/components.module.ts
+++ template-dashboard/src/app/views/components/components.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpModule } from '@angular/http';
+
 import { BackButtonComponent } from './buttons/back-button/back-button.component';
+import { ServertimestampComponent } from './servertimestamp/servertimestamp.component';
+
+import { StatusService } from '../../services/commons/status.service';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule, HttpModule
   ],
-  declarations: [BackButtonComponent],
-  exports: [BackButtonComponent]
+  declarations: [BackButtonComponent, ServertimestampComponent],
+  exports: [BackButtonComponent, ServertimestampComponent],
+  providers: [StatusService]
 })
 export class ComponentsModule { }
