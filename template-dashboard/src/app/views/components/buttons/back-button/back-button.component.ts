a5a5a16b7db86035a4938a2fefcdad5715ca6439
Javier Izquierdo Gomez <javier.izquierdo.gomez@gmail.com>
2018-08-12 11:37:54 +0200

    component module

---
+++ template-dashboard/src/app/views/components/buttons/back-button/back-button.component.ts
@@ -0,0 +1,20 @@
+import { Location } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-back-button',
+  templateUrl: './back-button.component.html',
+  styleUrls: ['./back-button.component.css']
+})
+export class BackButtonComponent implements OnInit {
+
+  constructor(private location: Location) { }
+
+  ngOnInit() {
+  }
+
+  goBack() {
+    this.location.back();
+  }
+
+}
---
+++ template-dashboard/src/app/views/components/components.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BackButtonComponent } from './buttons/back-button/back-button.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [BackButtonComponent]
+})
+export class ComponentsModule { }
