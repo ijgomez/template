ea14c13c96a78e48ad4f47241a6397953095e661
Javier Izquierdo Gomez <javier.izquierdo.gomez@gmail.com>
2018-08-13 00:11:59 +0200

    load data from server

--- template-dashboard/src/app/views/reports/reports-list/reports-list.component.ts
+++ template-dashboard/src/app/views/reports/reports-list/reports-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ReportsService } from '../../../services/reports/reports.service';
+import { ReportCriteria } from '../../../domain/reports/report-criteria';
 
 @Component({
   selector: 'app-reports-list',
@@ -13,13 +14,7 @@ export class ReportsListComponent implements OnInit {
   constructor(private reportsService: ReportsService) { }
 
   ngOnInit() {
-    this.data = [
-      {id: 1, name: 'Report 1', description: 'Description of the report 1', format: 'xls'},
-      {id: 2, name: 'Report 2', description: 'Description of the report 2', format: 'pdf'},
-      {id: 3, name: 'Report 3', description: 'Description of the report 3', format: 'csv'},
-      {id: 4, name: 'Report 4', description: 'Description of the report 4', format: 'xls'},
-      {id: 5, name: 'Report 5', description: 'Description of the report 5', format: 'xls'}
-    ];
+    this.reportsService.findByCriteria(new ReportCriteria()).subscribe(value => { this.data = value; });
   }
 
 }
