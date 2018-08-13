e93def62f6ef8010397d72f9012741d36eb7cf5f
Javier Izquierdo Gomez <javier.izquierdo.gomez@gmail.com>
2018-08-13 11:21:36 +0200

    load data

--- template-dashboard/src/app/views/reports/report/report.component.ts
+++ template-dashboard/src/app/views/reports/report/report.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { ReportsService } from '../../../services/reports/reports.service';
-import { FormGroup, Validators, FormBuilder } from '../../../../../node_modules/@angular/forms';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-report',
@@ -11,13 +12,41 @@ export class ReportComponent implements OnInit {
 
   reportForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder, private cd: ChangeDetectorRef, private reportsService: ReportsService) { }
+  constructor(
+    private activeRoute: ActivatedRoute,
+    private formBuilder: FormBuilder,
+    private cd: ChangeDetectorRef,
+    private reportsService: ReportsService
+  ) { }
 
   ngOnInit() {
+    this.initForm();
+    if (this.isEdit()) {
+      this.loadData();
+    }
+  }
+
+  initForm() {
     this.reportForm = this.formBuilder.group({
       name: ['', Validators.required],
       description: ['', Validators.required],
       file: [null, Validators.required]
+    });
+  }
+
+  isEdit(): Boolean {
+    const id = this.activeRoute.snapshot.params['id'];
+    return (id !== undefined);
+  }
+
+  loadData() {
+    const id = this.activeRoute.snapshot.params['id'];
+    this.reportsService.read(id).subscribe(r => {
+      this.reportForm.setValue({
+        name: r.name,
+        description: r.description,
+        file: null // TODO Pendiente de cargar el fichero.
+      });
     });
   }
 
