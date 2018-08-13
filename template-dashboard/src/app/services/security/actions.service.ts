911b3db744f4d05a0c17c630a579c5d27aeea0cd
Javier Izquierdo Gomez <javier.izquierdo.gomez@gmail.com>
2018-08-13 18:40:31 +0200

    action service

---
+++ template-dashboard/src/app/services/security/actions.service.ts
@@ -0,0 +1,116 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { Http, Response, RequestOptions, Headers, RequestMethod } from '@angular/http';
+import { Observable } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+import { Action } from '../../domain/security/action';
+import { ActionCriteria } from '../../domain/security/action-criteria';
+
+@Injectable()
+export class ActionsService {
+
+  private url = environment.urlBase + '/actions';
+
+  private headers = new Headers({'Content-type': 'application/json'});
+
+  constructor(private http: Http) { }
+
+  loadAll(): Observable<Action[]> {
+    const options = new RequestOptions({ headers: this.headers});
+
+    return this.http.get(this.url, options).pipe(
+      map((response: Response) => {
+        return response.json();
+      }),
+      catchError(this.handleError)
+    );
+  }
+
+  findByCriteria(criteria: ActionCriteria): Observable<Action[]> {
+    const options = new RequestOptions({ headers: this.headers});
+
+    return this.http.post(this.url + '/search', criteria, options).pipe(
+      map((response: Response) => {
+        return response.json();
+      }),
+      catchError(this.handleError)
+    );
+  }
+
+  countByCriteria(criteria: ActionCriteria): Observable<number> {
+    const options = new RequestOptions({ headers: this.headers});
+
+    return this.http.post(this.url + '/count', criteria, options).pipe(
+      map((response: Response) => {
+        return response.json();
+      }),
+      catchError(this.handleError)
+    );
+  }
+
+  create(action: Action): Observable<number> {
+    const options = new RequestOptions({ headers: this.headers});
+
+    return this.http.post(this.url, action, options).pipe(
+      map(success => success.status),
+      catchError(this.handleError)
+    );
+  }
+
+  read(id: number | string): Observable<Action> {
+    const options = new RequestOptions({ headers: this.headers});
+
+    return this.http.get(`${this.url}/${id}`, options).pipe(
+      map((response: Response) => {
+        return response.json();
+      }),
+      catchError(this.handleError)
+    );
+  }
+
+  update(action: Action): Observable<number> {
+    const options = new RequestOptions({ headers: this.headers});
+
+    return this.http.put(`${this.url}/${action.id}`, action, options).pipe(
+      map((response: Response) => {
+        return response.status;
+      }),
+      catchError(this.handleError)
+    );
+  }
+
+  delete(action: Action): Observable<number> {
+    const options = new RequestOptions({ headers: this.headers});
+
+    return this.http.delete(`${this.url}/${action.id}`, options).pipe(
+      map((response: Response) => {
+        return response.status;
+      }),
+      catchError(this.handleError)
+    );
+  }
+
+  export(criteria: ActionCriteria): Observable<any> {
+    const options = new RequestOptions({});
+
+    return this.http.post(this.url + '/export', criteria, options).pipe(
+      map((response: Response) => {
+        return response;
+      }),
+      catchError(this.handleError)
+    );
+  }
+
+  private handleError(error: Response | any) {
+    let errMsg: string;
+    if (error instanceof Response) {
+        const body = error.json() || '';
+        const err = body.error || JSON.stringify(body);
+        errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+    } else {
+        errMsg = error.message ? error.message : error.toString();
+    }
+    console.error('service error: ' + errMsg);
+    return Observable.throw(errMsg);
+  }
+}
