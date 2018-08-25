826ebd0e90884968736033f5e25ddc0609bbd8cf
Javier Izquierdo Gomez <javier.izquierdo.gomez@gmail.com>
2018-08-25 17:55:58 +0200

    menu and header

--- template-dashboard/src/app/app-routing.module.ts
+++ template-dashboard/src/app/app-routing.module.ts
@@ -3,7 +3,8 @@ import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './views/dashboard/dashboard.component';
 
 const routes: Routes = [
-    {path: '', component: DashboardComponent},
+    { path: '', redirectTo: 'home', pathMatch: 'full'},
+    { path: 'home', component: DashboardComponent},
     { path: 'reports',  loadChildren: 'app/views/reports/reports.module#ReportsModule'},
     { path: 'monitors', loadChildren: 'app/views/monitors/monitors.module#MonitorsModule'},
     { path: 'security', loadChildren: 'app/views/security/security.module#SecurityModule'},
