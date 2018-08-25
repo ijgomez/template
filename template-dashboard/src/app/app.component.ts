66f0071a4459b0a9de514bb41d4ba27db0f799f7
Javier Izquierdo Gomez <javier.izquierdo.gomez@gmail.com>
2018-08-25 21:47:04 +0200

    style

--- template-dashboard/src/app/app.component.ts
+++ template-dashboard/src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  
+
+  collapedSideBar: boolean;
+
+  constructor() { }
+
+  receiveCollapsed($event) {
+    this.collapedSideBar = $event;
+  }
 }
--- template-dashboard/src/app/views/layout/header/header.component.ts
+++ template-dashboard/src/app/views/layout/header/header.component.ts
@@ -9,9 +9,20 @@ export class HeaderComponent implements OnInit {
 
   title = 'template-dashboard';
 
+  pushRightClass = 'push-right';
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  isToggled(): boolean {
+    const dom: Element = document.querySelector('body');
+    return dom.classList.contains(this.pushRightClass);
+  }
+
+  toggleSidebar() {
+    const dom: any = document.querySelector('body');
+    dom.classList.toggle(this.pushRightClass);
+  }
 }
