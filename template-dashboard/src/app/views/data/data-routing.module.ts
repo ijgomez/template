70fc72f603013bc8053c62f0705adf10af9c2ce6
Javier Izquierdo Gomez <javier.izquierdo.gomez@gmail.com>
2018-09-01 20:24:38 +0200

    scripts list view

--- template-dashboard/src/app/views/data/data-routing.module.ts
+++ template-dashboard/src/app/views/data/data-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DataHomeComponent } from './data-home/data-home.component';
 import { TableEditorComponent } from './table-editor/table-editor.component';
+import { ScriptListComponent } from './script-list/script-list.component';
 
 const routes: Routes = [
     { path: '', component: DataHomeComponent, children: [
         { path: '', component: TableEditorComponent},
         { path: 'editor', pathMatch: 'full', redirectTo: ''},
+        { path: 'scripts', component: ScriptListComponent}
       ] }
 ];
   
--- template-dashboard/src/app/views/data/data.module.ts
+++ template-dashboard/src/app/views/data/data.module.ts
@@ -4,12 +4,13 @@ import { DataRoutingModule } from './data-routing.module';
 import { DataHomeComponent } from './data-home/data-home.component';
 import { TableEditorComponent } from './table-editor/table-editor.component';
 import { ComponentsModule } from '../components/components.module';
+import { ScriptListComponent } from './script-list/script-list.component';
 
 @NgModule({
   imports: [
     CommonModule,
     DataRoutingModule, ComponentsModule
   ],
-  declarations: [DataHomeComponent, TableEditorComponent]
+  declarations: [DataHomeComponent, TableEditorComponent, ScriptListComponent]
 })
 export class DataModule { }
