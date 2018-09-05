2e067860b1acae811d83336fcde008a938c1ad1f
Javier Izquierdo <javier.izquierdo.gomez@gmail.com>
2018-09-05 23:13:51 +0200

    correct error

--- template-app/core/src/main/java/org/myorganization/template/core/domain/system/properties/PropertyRepository.java
+++ template-app/core/src/main/java/org/myorganization/template/core/domain/system/properties/PropertyRepository.java
@@ -1,7 +1,12 @@
 package org.myorganization.template.core.domain.system.properties;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface PropertyRepository extends PagingAndSortingRepository<Property, Long>, PropertyRepositoryQueries {
+	
+	@Query("SELECT p FROM Property p WHERE LOWER(p.property) = LOWER(:property)")
+	public Property findByProperty(@Param("property") String property);
 	
 }
--- template-app/core/src/main/java/org/myorganization/template/core/domain/system/properties/PropertyRepositoryQueries.java
+++ template-app/core/src/main/java/org/myorganization/template/core/domain/system/properties/PropertyRepositoryQueries.java
@@ -1,12 +1,9 @@
 package org.myorganization.template.core.domain.system.properties;
 
 import org.myorganization.template.core.domain.base.RepositoryQueries;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface PropertyRepositoryQueries extends RepositoryQueries<Property, PropertyCriteria> {
 
-	@Query("SELECT p FROM Property p WHERE LOWER(p.property) = LOWER(:property)")
-	public Property findByProperty(@Param("property") String property);
+	
 	
 }
