27b2b14ea5a0587501aff2826b39e522a4cc98e9
Javier Izquierdo Gomez <javier.izquierdo.gomez@gmail.com>
2022-03-01 22:22:07 +0100

    docker

--- template-docker/postgresql/config/init.sql
+++ template-docker/postgresql/config/init.sql
@@ -3,5 +3,5 @@ CREATE USER template_admin;
 CREATE DATABASE template;
 GRANT ALL PRIVILEGES ON DATABASE template TO template_admin;
 
-CREATE TABLESPACE template_data LOCATION '/data/TEMPLATE_DATA';
-CREATE TABLESPACE template_index LOCATION '/data/TEMPLATE_INDEX';
+CREATE TABLESPACE template_data OWNER template_admin LOCATION '/data/template_data';
+CREATE TABLESPACE template_index OWNER template_admin LOCATION '/data/template_index';
